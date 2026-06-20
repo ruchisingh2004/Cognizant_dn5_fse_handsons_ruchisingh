699b22cbd7dfabb16565840b3696c8c638fc0703
Ruchi Singh <riya.ruchi09@gmail.com>
2026-06-21 00:13:55 +0530

    Completed Financial Forecasting

---
+++ Deepskilling/DataStructuresAlgorithms/FinancialForecasting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static double Forecast(double amount,
+                           double growthRate,
+                           int years)
+    {
+        if (years == 0)
+            return amount;
+
+        return Forecast(
+            amount * (1 + growthRate),
+            growthRate,
+            years - 1);
+    }
+
+    static void Main()
+    {
+        double futureValue =
+            Forecast(10000, 0.10, 5);
+
+        Console.WriteLine(
+            "Future Value = " + futureValue);
+    }
+}
