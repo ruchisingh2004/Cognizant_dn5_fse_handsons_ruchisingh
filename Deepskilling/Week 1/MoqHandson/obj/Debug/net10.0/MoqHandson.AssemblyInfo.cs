18c854142f1e29558c7097b83d07c4e4775521b4
Ruchi Singh <riya.ruchi09@gmail.com>
2026-07-17 21:13:43 +0530

    Completed Week 1 Hands-on

--- Deepskilling/Week 1/MoqHandson/obj/Debug/net10.0/MoqHandson.AssemblyInfo.cs
+++ Deepskilling/Week 1/MoqHandson/obj/Debug/net10.0/MoqHandson.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoqHandson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb5c2f8cb024297ffdcbe2347c2227f2d241cc92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+859932012d0d07739736f3a78ef54f897d7a800b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoqHandson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoqHandson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
